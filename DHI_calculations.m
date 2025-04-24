f07ae15e1967d4f29d5720e2f50e11195ee249eb
kwetzaal <kirazhong@gmail.com>
2025-04-24 11:58:46 -0400

    Update DHI_calculations.m
    fixed some formatting issues

--- DHI_calculations.m
+++ DHI_calculations.m
@@ -1,34 +1,57 @@
-function dhi = DHI_Calculations(ghi_data, dni_data, zenith_ang)
-    %% Calculate DHI %%
+function dni = dniCalculations(solar_long, optical_depth, zenith_ang)
+    %% Initialize constants %%
+    mean_irr = 588.6;  % Mean solar irradiance
+    eccentricity = 0.0934;
+    Ls_p = 251;  % Convert degrees to radians
+
+    %% Calculate Top of Atmosphere (TOA) irradiance %%
+    toa = mean_irr * ((1 + (eccentricity * cosd(solar_long - Ls_p)) / (1 - eccentricity^2))^2);
+
+    %% Calculate DNI %%
 
     % calculate cos(zenith_ang)
-    sz_1 = size(zenith_ang.SolarZenithAngle);
+    sz_1 = size(zenith_ang);
     zenithAngDouble = zeros(sz_1(1), sz_1(2), sz_1(3));
     for i = 1:sz_1(1)
         for j = 1:sz_1(2)
             for k = 1:sz_1(3)
-                if zenith_ang.SolarZenithAngle{i,j,k} < 0         
-                    zenith_ang.SolarZenithAngle{i,j,k} = 0;
+                zen = str2double(zenith_ang{i,j,k});
+                if zen > 90         
+                    zen = 90;
                 end
-                zen = str2double(zenith_ang.SolarZenithAngle{i,j,k});
+                if zen < 0         
+                    zen = 0;
+                end
                 cosZenith = cosd(zen);
                 zenithAngDouble(i,j,k) = cosZenith;
             end
         end
     end
     
-    dniDouble = str2double(dni_data);
-    ghiDouble = str2double(ghi_data);
+    % calculate exp(-optical_depth)
+    sz_2 = size(optical_depth);
+    opticalDepthDouble = zeros(sz_2(1), sz_2(2), sz_2(3));
+    for l = 1:sz_2(1)
+        for m = 1:sz_2(2)
+            for n = 1:sz_2(3)
+                expOpticalDepth = exp(str2double(optical_depth{l,m,n}));
+                opticalDepthDouble(l,m,n) = expOpticalDepth;
+            end
+        end
+    end
     
-    dhi = double((ghiDouble-dniDouble).*zenithAngDouble);
+    % calculates DNI 
+    surf_irr = round(toa .* zenithAngDouble .* opticalDepthDouble, 6);
+    dni = reshape(surf_irr, 25, 64, 48, 1);
 end
+
 %% INITIALIZE DATA %%
 
-% Global Horizontal Irradiance 
-folder = 'C:\Users\kiraz\OneDrive\Documents\School\Purdue Freshman\VIP ABC-PV\Mars Irradiance(Matlab)\InciddentSolarFluxOnHorizontalSurface\'; % opens selected folder (Optical Depth)
+% Optical Depth
+folder = 'C:\Users\kiraz\OneDrive\Documents\School\Purdue Freshman\VIP ABC-PV\Mars Irradiance(Matlab)\DailyMeanDustColumnVisibleOpticalDepthAboveSurface\'; % opens selected folder (Optical Depth)
 files = dir(fullfile(folder, '*.mat'));  % List all .mat files
 
-ghiData = [];
+opticalDepthData = cell(25, 64, 48, 0);
 
 % loops through all .mat files in the folder
 for k = 1:length(files)
@@ -37,7 +60,9 @@ for k = 1:length(files)
         message = printf('%s does not exist', filepath);
     else
         fileData = load(filepath)'; % load .mat file
-        ghiData = cat(4, ghiData, fileData); % concatonates to 4th Dimension of struct (time, lat, long, solar long)
+        sz = size(fileData.DailyMeanDustColumnVisibleOpticalDepthAboveSurface);
+        fileData4D = reshape(fileData.DailyMeanDustColumnVisibleOpticalDepthAboveSurface, sz(1), sz(2), sz(3), 1); % turns data into 4D array
+        opticalDepthData = cat(4, opticalDepthData, fileData.DailyMeanDustColumnVisibleOpticalDepthAboveSurface); % concatonates to 4th Dimension of struct (time, lat, long, solar long)
     end 
 end
 
@@ -45,7 +70,7 @@ end
 folder = 'C:\Users\kiraz\OneDrive\Documents\School\Purdue Freshman\VIP ABC-PV\Mars Irradiance(Matlab)\SolarZenithAngle\'; % opens selected folder (Zenith Angle)
 files = dir(fullfile(folder, '*.mat'));  % List all .mat files
 
-solarZenithData = [];
+solarZenithData = cell(25, 64, 48, 0);
 
 % loops through all .mat files in the folder
 for k = 1:length(files)
@@ -54,21 +79,26 @@ for k = 1:length(files)
         message = printf('%s does not exist', filepath);
     else
         fileData = load(filepath)'; % load .mat file
-        solarZenithData = cat(4, solarZenithData, fileData); % concatonates to 4th Dimension of struct (time, lat, long, solar long)
+        sz = size(fileData.SolarZenithAngle);
+        fileData4D = reshape(fileData.SolarZenithAngle, sz(1), sz(2), sz(3), 1); % turns data into 4D array
+        solarZenithData = cat(4, solarZenithData, fileData.SolarZenithAngle); % concatonates to 4th Dimension of struct (time, lat, long, solar long)
     end 
 end
 
-% Direct Normal Irradiance
-dniData = load('DNI_Data.mat')';
-
+% Solar Longitude
+solarLongitudeData = 15:30:360;
+disp(solarLongitudeData)
 
 %% CALCULATIONS %%
-dhiData = [];
+
+dniData = zeros(25, 64, 48, 0);
 i = 1;
 while i <= length(solarLongitudeData)
-    dhiDataCalc = DHI_Calculations(ghiData( :, :, :, i), dniData( : , : , : , i), solarZenithData( : , : , :, i));
-    dhiData = cat(4, dhiData, dhiDataCalc);
+    dniDataForSolarLong = dniCalculations(solarLongitudeData(i), opticalDepthData( : , : , : , i), solarZenithData( : , : , :, i));
+    disp(size(dniDataForSolarLong))
+    dniData = cat(4, dniData, dniDataForSolarLong);
     i = i + 1;
 end
-disp(dhiData)
-%save('DHI_Data','dhiData')
+disp(dniData)
+
+save('DNI_Data','dniData')
